8a5ed20f36e9a243efe65a345fe0df98b297a2c1
aman85662 <118710449+aman85662@users.noreply.github.com>
2025-09-28 23:03:18 +0530

    Navigation-issue resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { ScrollTrigger, SplitText } from "gsap/all"
 import gsap from "gsap"
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import Cocktail from "./components/cocktail"
-import About from "./components/About.jsx"
-import Art from "./components/Art"
-import Menu from "./components/Menu"
-import Contact from "./components/Contact"
+import Navbar from "./components/NavbarPage.jsx"
+import Hero from "./components/HeroPage.jsx"
+import Cocktail from "./components/CocktailPage.jsx"
+import About from "./components/AboutPage.jsx"
+import Art from "./components/ArtPage.jsx"
+import Menu from "./components/MenuPage.jsx"
+import Contact from "./components/ContactPage.jsx"
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
